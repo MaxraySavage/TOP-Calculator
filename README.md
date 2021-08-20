48a7b1e0e6fabebdfc0f480bf53495481a7eadb1
MaxraySavage <MaxraySavage@users.noreply.github.com>
2021-08-20 16:30:17 -0400

    implements number input

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ Calculator project from [The Odin Project](https://www.theodinproject.com/paths/
 TODO:
 - [x] Operation functions (add, subtract, multiply, divide)
 - [x] Create a new function `operate` that takes an operator and 2 numbers and then calls one of the above functions on the numbers
-- [ ] Calculator design without functionality
-- [ ] Create the functions that populate the display when you click the number
+- [x] Calculator design without functionality
+- [x] Create the functions that populate the display when you click the number
 - [ ] Make the calculator work!
 - [ ] Include decimal button and backspace 
+
+###
+Credits:
+Font from https://www.dafont.com/ds-digital.font
--- script.js
+++ script.js
@@ -23,3 +23,103 @@ function operate (a, b, operator) {
             return divide(a, b);
     }
 }
+
+const calcDisplay = {
+    isOn: false,
+    display: document.getElementById('current-number'),
+    signDisplay: document.getElementById('sign-indicator'),
+    toggleSign() {
+        if(this.signDisplay.textContent === '') {
+            this.signDisplay.textContent = '-';
+        } else {
+            this.signDisplay.textContent = '';
+        }
+    },
+    getDisplay() {
+        return this.display.textContent;
+    },
+    getDisplayAsNumber() {
+        let displayNumber = Number(this.display.textContent);
+        if(this.signDisplay.textContent === '-'){
+            displayNumber *= -1;
+        }
+        return displayNumber;
+    },
+    setDisplay(s) {
+        this.display.textContent = s;
+    },
+    setDisplayToNumber (num) {
+        if(Math.abs(num) >= 10**12) {
+            this.setDisplay('ERR-Overflow');   
+            this.signDisplay.textContent = '';
+        } else {
+            const numAsString = String(Math.abs(num));
+            if(numAsString.includes('.')) {
+                this.setDisplay(numAsString.slice(0,13));
+            } else {
+                this.setDisplay(numAsString.slice(0,12));
+            }
+            if(num < 0) {
+                this.signDisplay.textContent = '-';
+            } else {
+                this.signDisplay.textContent = '';
+            }
+        }
+    },
+    displayIsFull() {
+        const currentDisplay = this.getDisplay();
+        const maxDisplayLength = currentDisplay.includes('.') ? 13: 12;
+        if(currentDisplay.length >= maxDisplayLength) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+    inputDigit(num) {
+        if(!this.displayIsFull()) {
+            const currentDisplay = this.getDisplay();
+            if(currentDisplay !== '0') {
+                this.setDisplay(currentDisplay + num);
+            } else {
+                this.setDisplay(num);
+            }
+        }
+    },
+    inputDecimal() {
+        if(!this.getDisplay.includes('.') && !this.displayIsFull()) {
+            this.setDisplay(currentDisplay + '.');
+        }
+    }
+    turnOn(){
+        if(!this.isOn) {
+            this.setDisplay(-888888888888)
+            document.getElementById('number-display').style.animationPlayState = 'running';
+            setTimeout(()=>{
+                this.setDisplay('0');
+                this.signDisplay.innerText='';
+                this.isOn = true;
+            }, 4000);
+        }
+    },
+};
+
+window.addEventListener('load', () => {
+    for(let i=0; i <= 9; i++){
+        document.getElementById(`button-${i}`).addEventListener('click', ()=>{
+            calcDisplay.inputDigit(i);
+        });
+    }
+    document.getElementById('button-plus-minus').addEventListener('click',()=>{
+        calcDisplay.toggleSign();
+    });
+    document.getElementById('button-on-clr').addEventListener('click',()=>{
+        if(!calcDisplay.isOn){
+            calcDisplay.turnOn();
+        } else {
+            calcDisplay.setDisplayToNumber(0)
+        } 
+    });
+    document.getElementById('button-decimal').addEventListener('click',()=>{
+        calcDisplay.inputDecimal();
+    });
+});
